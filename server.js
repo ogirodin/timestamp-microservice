04b889e7c68f6de984918a63dd5c5c795b85add4
Olivier Girodin <ogirodin@gmail.com>
2022-01-18 22:51:53 +0100

    adding route to display unix and GMT time

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,12 +19,45 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
 
+// your first API endpoint...
+app.get("/api/:time", function (req, res) {
+  let out;
+  let date;
+  if (/^\d+$/.test(req.params.time)) {
+    let intTime = parseInt(req.params.time, 10);
+    date = new Date(intTime);
+    out = {
+      "unix": intTime,
+      "utc": date.toGMTString(),
+    };
+  } else if (/^(\d{4})-\d{2}-\d{2}$/.test(req.params.time)) {
+    let date = getDateFromText(req.params.time);
+    out = {
+      "unix": date.getTime(),
+      "utc": date.toGMTString('utc', { timeZone: 'Europe/London' }),
+    };
+  }
+  res.json(out);
+});
+
+// date should be formated as YYYY-MM-DD
+function getDateFromText(textDate) {
+  let date = new Date();
+  date.setFullYear(textDate.replace(/^(\d{4}).*/, '$1'));
+  date.setMonth(textDate.replace(/\d{4}-(\d{2}).*/, '$1') - 1);
+  date.setDate(textDate.replace(/\d{4}-\d{2}-(\d{2})/, '$1'));
+  date.setHours(0);
+  date.setMinutes(0);
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+  return date;
+}
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
